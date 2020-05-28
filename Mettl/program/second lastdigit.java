64d5e8b173f0ee86ec6d1da06d7c9f5f80831024
Uma Maheshwari.M <60137761+Uma1305@users.noreply.github.com>
2020-05-28 12:00:45 +0530

    Add files via upload

---
+++ Mettl/program/second lastdigit.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+//read only region start
+  class UserMainCode
+{
+            public int seconfLastDigitof(int input1){
+              //read only region end
+                  input1=((input%100)/10);
+                   if(input1<=0){
+                       input*=-1;
+                   }
+
+                   return (input1);
+         }
+ }
