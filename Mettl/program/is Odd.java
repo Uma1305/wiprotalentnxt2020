23a1744e1d80c9bc9cbe4401fe9c3c77c20b379d
Uma Maheshwari.M <60137761+Uma1305@users.noreply.github.com>
2020-05-28 11:56:37 +0530

    Add files via upload

---
+++ Mettl/program/is Odd.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+//read only region start
+  class UserMainCode
+{
+            public int isOdd(int input1){
+              //read only region end
+                if(input1%2!=0){
+                      return 2;
+               }
+                else{
+                return 1;
+               }
+           }
+  }
---
+++ Mettl/program/last digitof.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+//read only region start
+  class UserMainCode
+{
+            public int lastDigitof(int input1){
+              //read only region end
+                  input1=input%10;
+                   if(input1<=0){
+                       input*=-1;
+                   }
+
+                   return (input1);
+         }
+ }
