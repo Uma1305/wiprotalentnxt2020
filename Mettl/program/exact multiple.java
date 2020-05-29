27b1101cd63943de0463e8aadcc141893e046e82
Uma Maheshwari.M <60137761+Uma1305@users.noreply.github.com>
2020-05-29 12:28:18 +0530

    Add files via upload

---
+++ Mettl/program/exact multiple.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+class UserMainCode
+{
+ public int isMultiple(int input1,int input2){
+ {
+  if(input==0||input2==0){
+      return 3;
+  }
+  else if ((input1%input2)!=0)
+   {
+     return 1;
+   }
+  else
+   {
+     return 2;
+   }
