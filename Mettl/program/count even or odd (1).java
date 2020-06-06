063c67c458f0a22930b72184f4b48efd7325778f
Uma Maheshwari.M <60137761+Uma1305@users.noreply.github.com>
2020-06-06 15:59:53 +0530

    Add files via upload

---
+++ Mettl/program/count even or odd (1).java
@@ -0,0 +1,44 @@
+import java.io.*;
+import  java.util.*;
+
+class CountEvensOdds{
+	public int countEvensOdds(int input1,int input2,int input3,int input4,int input5,String input6){
+int count=0;
+if(input6=equalsIgnoreCase("even")){
+    if(input1<0)input1=(-1)*input1;
+    if(input2<0)input2=(-1)*input2;
+    if(input3<0)input3=(-1)*input3;
+    if(input4<0)input4=(-1)*input4;
+    if(input5<0)input5=(-1)*input5;
+    if(input1%2==0)
+    count++;
+    if(input2%2==0)
+    count++;
+    if(input3%2==0)
+    count++;
+    if(input4%2==0)
+    count++;
+    if(input5%2==0)
+    count++;
+    }
+    else if(input6=equalsIgnoreCase("odd"))
+    {
+     if(input1<0)input1=(-1)*input1;
+    if(input2<0)input2=(-1)*input2;
+    if(input3<0)input3=(-1)*input3;
+    if(input4<0)input4=(-1)*input4;
+    if(input5<0)input5=(-1)*input5;
+    if(input1%2!=0)
+    count++;
+    if(input2%2!=0)
+    count++;
+    if(input3%2!=0)
+    count++;
+    if(input4%2!=0)
+    count++;
+    if(input5%2!=0)
+    count++;   
+    }
+    return count;
+    }
+    }
