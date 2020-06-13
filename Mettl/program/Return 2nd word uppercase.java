f2f6ce0ccd21bfaf53960c0fc106b14fec0bf820
Uma Maheshwari.M <60137761+Uma1305@users.noreply.github.com>
2020-06-13 20:57:54 +0530

    Add files via upload

---
+++ Mettl/program/Return 2nd word uppercase.java
@@ -0,0 +1,13 @@
+import.java.io.*;
+import.java.util.*;
+class UserMainCode{
+public string secondWordUpperCase(string input1)
+{
+String s[]=input1.split("");
+        if(s.length==1)
+        return "LESS";
+        String s1=s[1];
+        s1=s1.toUpperCase();
+        return s1;
+}
+}
